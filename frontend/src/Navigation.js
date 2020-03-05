7a60782672025d95670506472b7c5cc323084039
Martin Kim <martinjwkim@gmail.com>
2020-03-05 13:57:29 -0800

    changed navbar color

--- frontend/src/Navigation.js
+++ frontend/src/Navigation.js
@@ -19,13 +19,13 @@ function Navigation() {
           <NavLink className="nav-link" exact to="/companies">Companies</NavLink>
           <NavLink className="nav-link" exact to="/jobs">Jobs</NavLink>
           <NavLink className="nav-link" exact to="/profile">Profile</NavLink>
-          <NavLink className="nav-link" to="/" onClick={handleLogOut}>Log Out</NavLink>
+          <NavLink className="nav-link" to="/ " onClick={handleLogOut}>Log Out</NavLink>
         </div>
       : <NavLink className="nav-link" exact to="/login">Log In</NavLink>
   )
 
   return (
-    <nav className="navbar navbar-light bg-light">
+    <nav className="navbar navbar-light bg-light bg-white">
       <NavLink className="nav-link" exact to="/">Jobly</NavLink>
       {showNavLinks()}
     </nav>
