2e65115e4262303a281963c0b706b33820420ee6
ngjamesng <just.james.ng@gmail.com>
2020-07-01 11:47:16 -0700

    add default count to SkeletonCArds

--- frontend/src/SkeletonCards.js
+++ frontend/src/SkeletonCards.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import SkeletonCard from "./SkeletonCard";
 
-function SkeletonCards({ count }) {
-let cards = [];
-for(let i = 0; i < count; i++){
-  cards.push(<SkeletonCard key={i}/>);
-}
+function SkeletonCards({ count = 5 }) {
+  let cards = [];
+  for (let i = 0; i < count; i++) {
+    cards.push(<SkeletonCard key={i} />);
+  }
   return (
     <>
-      {cards.map(c=> c )}
+      {cards.map(c => c)}
     </>
   )
 }
