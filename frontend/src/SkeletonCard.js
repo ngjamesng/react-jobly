68cfd1c72f0568dd04042dd0fec7778233cd49e2
ngjamesng <just.james.ng@gmail.com>
2020-07-01 11:47:46 -0700

    fix card name width

--- frontend/src/SkeletonCard.js
+++ frontend/src/SkeletonCard.js
@@ -6,10 +6,10 @@ function SkeletonCard(){
 return (
   <section className="card CompanyCard" >
   <div className="CompanyCard-NameImg">
-    <Skeleton className="CompanyCard-Name" height={25} width={300}/>
+    <Skeleton className="CompanyCard-Name" height={25} width={200}/>
     <Skeleton height={50} width={50} className="card-img-top Company-Img"/>
   </div>
-  <Skeleton count={2}/>
+  <Skeleton count={3}/>
 </section>
 )
 }
