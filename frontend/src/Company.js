edc4d9908851463defddcbcf043ff6fb4b8cc6ed
ngjamesng <just.james.ng@gmail.com>
2020-07-01 11:46:35 -0700

    implement skeleton loading on company

--- frontend/src/Company.js
+++ frontend/src/Company.js
@@ -4,7 +4,8 @@ import { useParams, Redirect } from "react-router-dom";
 import JoblyApi from "./JoblyApi";
 import NotFound from "./NotFound";
 import UserContext from "./userContext";
-
+import Skeleton from "react-loading-skeleton";
+import SkeleonCards from "./SkeletonCards";
 
 function Company() {
   const { handle } = useParams();
@@ -29,10 +30,18 @@ function Company() {
     ))
   );
 
+  const SkeletonCompany = () => (
+    <div className="container">
+    <Skeleton style={{margin: "30px auto"}} height={50} width={300} />
+    <Skeleton count={2} />
+    <p>{company.description}</p>
+    <SkeleonCards count={5}/>
+  </div>
+  )
 
   return (
     (!company.handle)
-      ? <NotFound />
+      ? <SkeletonCompany />
       : (
         <div className="container">
           <h2 style={{margin: "30px auto"}}>{company.name}</h2>
